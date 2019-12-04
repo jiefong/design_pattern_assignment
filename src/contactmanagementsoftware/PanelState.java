2e057f0ef6dd2b90ae3889ca836cb8f8f5c7a762
Leanne Tan <leannetan97@hotmail.com>
2019-12-05 00:44:03 +0800

    Recommit by adding back PanelState

---
+++ src/contactmanagementsoftware/PanelState.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactmanagementsoftware;
+
+/**
+ *
+ * @author jiefo
+ */
+public abstract class PanelState {
+    
+    MUI mui;
+    
+    public PanelState(MUI mui){
+        this.mui = mui;
+    }
+    
+    public abstract void setDescription();
+}
